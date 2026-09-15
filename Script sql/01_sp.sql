594726d0ef42375b4025828226305047a932f565
Angelo Piedra <angelopiedra2006@gmail.com>
2026-09-15 02:10:11 -0600

    Creacion del primer procedure para obtener todos los productos

---
+++ Script sql/01_sp.sql
@@ -0,0 +1,5 @@
+CREATE PROCEDURE sp_GetAllProducts AS
+BEGIN
+    SELECT p.ProductID, p.Name, p.ProductNumber, p.Color, p.StandardCost, p.ListPrice, p.ProductSubcategoryID, p.SellStartDate 
+    FROM Production.Product p;
+END;
