c2186d649ce004eaee7b42121d3b09ba7cf35b16
Angelo Piedra <angelopiedra2006@gmail.com>
2026-09-15 22:07:23 -0600

    Implement CRUD operations for products: add, update, and delete functionality

--- routes/products.js
+++ routes/products.js
@@ -12,4 +12,62 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.get("/with-category", async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().execute("sp_GetProductWithCategory");
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const { name, productNumber, color, standardCost, listPrice, subcategoryID } = req.body;
+        const pool = await poolPromise;
+        const result = await pool.request()
+            .input("Name", sql.NVarChar, name)
+            .input("ProductNumber", sql.NVarChar, productNumber)
+            .input("Color", sql.NVarChar, color || null)
+            .input("StandardCost", sql.Decimal(18, 2), standardCost)
+            .input("ListPrice", sql.Decimal(18, 2), listPrice)
+            .input("ProductSubcategoryID", sql.Int, subcategoryID || null)
+            .execute("sp_InsertProduct");
+        res.json({ message: "Producto insertado correctamente", productId: result.recordset[0].NewProductID });
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+        const { productId, name, color, standardCost, listPrice, subcategoryID } = req.body;
+        const pool = await poolPromise;
+        const result = await pool.request()
+            .input("ProductID", sql.Int, req.params.id)
+            .input("Name", sql.NVarChar(50), name)
+            .input("Color", sql.NVarChar(15), color || null)
+            .input("StandardCost", sql.Decimal(18, 2), standardCost)
+            .input("ListPrice", sql.Decimal(18, 2), listPrice)
+            .input("ProductSubcategoryID", sql.Int, subcategoryID || null)
+            .execute("sp_UpdateProduct");
+        res.json({ message: "Producto actualizado correctamente" });
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const pool = await poolPromise;
+        await pool.request()
+            .input("ProductID", sql.Int, req.params.id)
+            .execute("sp_DeleteProduct");
+        res.json({ message: "Producto eliminado correctamente" });
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
 module.exports = router;
